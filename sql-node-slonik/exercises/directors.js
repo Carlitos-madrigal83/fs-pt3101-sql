cd6630997813956985cdd108d2165f60e6105b51
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-05-16 15:51:56 +0200

    Ejercicios 1 y 2 OK

--- sql-node-slonik/exercises/directors.js
+++ sql-node-slonik/exercises/directors.js
@@ -4,34 +4,64 @@ const { sql } = require('slonik')
 module.exports = {
     // Your directors queries
     async q1() {
+        const resultado =  await db.query( sql`
+        SELECT name FROM directors WHERE name != ''
+        `)
+        return resultado.rows
         // README - Directors.1
     },
 
     async q2() {
+        return await db.query( sql`
+        SELECT query_name, nickname FROM directors
+        `
+        )
         // README - Directors.2
     },
 
     async q3() {
+        return await db.query( sql`
+        SELECT pic, nickname FROM directors WHERE nickname != '' 
+        `
+        )
         // README - Directors.3
     },
 
     async q4() {
+        return await db.query( sql`
+        SELECT query_name, nationality FROM directors WHERE nationality LIKE '%canadiense%'         `
+        )
         // README - Directors.4
     },
 
     async q5() {
+        return await db.query( sql`
+        SELECT query_name, nationality FROM directors 
+        WHERE nationality IN ('británica,estadounidense', 'estadounidense,británico')
+        `)
         // README - Directors.5
     },
 
     async q6() {
+        return await db.query( sql`
+        SELECT query_name, nationality, roles FROM directors WHERE roles = 'ajedrecistas' 
+        `)
         // README - Directors.6
     },
 
     async q7() {
+        return await db.query( sql`
+        SELECT query_name, name, nationality FROM directors WHERE nationality LIKE '%,%'
+        `)
         // README - Directors.7
     },
 
     async q8() {
+        return await db.query( sql`
+        SELECT query_name, roles
+        FROM directors
+        WHERE roles LIKE '%,%,%'
+        `)
         // README - Directors.8
     },
 }
--- sql-node-slonik/exercises/movies.js
+++ sql-node-slonik/exercises/movies.js
@@ -4,62 +4,138 @@ const { sql } = require('slonik')
 module.exports = {
     // Your movies queries
     async q9() {
+        return await db.query( sql`
+        SELECT title FROM movies WHERE title IS NOT NULL
+        `)
         // README - Movies.9
     },
 
     async q10() {
+        return await db.query( sql`
+        SELECT title, mpaa_rating FROM movies WHERE mpaa_rating IS NOT NULL
+        `)
         // README - Movies.10
     },
 
     async q11() {
+        return await db.query( sql`
+        SELECT title, production_budget, distributor
+        FROM movies
+        WHERE production_budget < 500000
+        `)
         // README - Movies.11
     },
 
     async q12() {
+        return await db.query( sql`
+        SELECT title, major_genre, production_budget 
+        FROM movies
+        WHERE production_budget IS NOT NULL
+        ORDER BY production_budget DESC
+        LIMIT 10
+        `)
         // README - Movies.12
     },
 
     async q13() {
+        return await db.query( sql`
+        SELECT title, source FROM movies WHERE source = 'Remake'
+        `)
         // README - Movies.13
     },
 
     async q14() {
+        return await db.query( sql`
+        SELECT title, distributor, imdb_rating FROM movies WHERE imdb_rating IS NOT NULL
+        `)
         // README - Movies.14
     },
 
     async q15() {
+        return await db.query( sql`
+        SELECT title, rotten_tomatoes_rating 
+        FROM movies
+        WHERE rotten_tomatoes_rating IS NOT NULL
+        ORDER BY rotten_tomatoes_rating DESC
+        LIMIT 100
+        `)
         // README - Movies.15
     },
 
     async q16() {
+        return await db.query( sql`
+        SELECT title, major_genre, imdb_rating, imdb_votes
+        FROM movies
+        WHERE imdb_rating IS NOT NULL AND imdb_votes IS NOT NULL
+        ORDER BY imdb_rating DESC, imdb_votes DESC
+        `)
         // README - Movies.16
     },
 
     async q17() {
+        const result17 = await db.query(sql`
+        SELECT SUM(production_budget)
+        FROM movies
+        WHERE mpaa_rating = 'Not Rated' AND title IS NOT NULL
+        `)
+        return result17.rows         
         // README - Movies.17
     },
 
     async q18() {
+        return await db.query( sql`
+        SELECT title, release_date 
+        FROM movies
+        WHERE release_date > CURRENT_DATE
+        ORDER BY release_date DESC
+        `)
         // README - Movies.18
     },
 
     async q19() {
+        return await db.query( sql`
+        SELECT title, us_gross, release_date 
+        FROM movies
+        WHERE release_date BETWEEN '1950-01-01' AND '1980-12-31'
+        `)
         // README - Movies.19
     },
 
     async q20() {
+        return await db.query( sql`
+        SELECT title, us_gross, worldwide_gross
+        FROM movies
+        WHERE us_gross = 0 OR worldwide_gross = 0
+        `)
         // README - Movies.20
     },
 
     async q21() {
+        return await db.query( sql`
+        SELECT title, us_gross, worldwide_gross
+        FROM movies
+        ORDER BY us_gross
+        LIMIT 50
+        `)
         // README - Movies.21
     },
 
     async q22() {
+        return await db.query( sql`
+        SELECT title, source
+        FROM movies 
+        WHERE title LIKE 'F%'
+        `)
         // README - Movies.22
     },
 
     async q23() {
+        return await db.query( sql`
+        SELECT distributor, production_budget, creative_type, major_genre
+        FROM movies
+        WHERE production_budget > 10000000 
+        AND distributor = 'Sony Pictures'
+        `)
         // README - Movies.23
     },
 }
